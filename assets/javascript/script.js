acd4de8f0df26537d67c195f82167164be0aadd5
Thomas21119 <thomas.wittwer2018@gmail.com>
2021-09-28 19:08:27 +1000

    testing

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,17 +1,25 @@
-var random = [
-    'one',
-    'two',
-]
+var options = {
+    method: 'GET',
+    url: 'https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/autosuggest/v1.0/UK/GBP/en-GB/',
+    params: {query: 'Stockholm'},
+    headers: {
+      'x-rapidapi-host': 'skyscanner-skyscanner-flight-search-v1.p.rapidapi.com',
+      'x-rapidapi-key': '6fbd183997msh60b69efce9d0b37p193b51jsn4d762a8a5370'
+    }
+  };
 
 // trying to use geonames.api to get all city names for a pretext function but am struggling if you can think of another api lets do that
 $(function () {
-    cityApiUrl = 'http://api.geonames.org/searchJSON?&username=hypnoticwolf987';
-
-    fetch(cityApiUrl)
+    fetch(options)
+    
     .then(function(response){
         if (response.ok) {
             response.json().then(function (data) {
-                console.log(data.geonames)
+                console.log(data)
+                console.log(data.Places[0])
+                $( "#toDestination" ).autocomplete({
+                    source: data.Places[0]
+                  });
             })
         } else {
             console.log("error: " + response.status)
